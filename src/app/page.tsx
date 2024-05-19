e145eb0d253a3c0e9ba4823d1674a16ca2aeea61
Kevin <kevinsilvarj10@gmail.com>
2024-05-19 15:12:09 -0300

    feat: Finish marketing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,12 @@ import { UnderlineIcon } from '@/components/icons/underline-icon'
 import { MaxWidthWrapper } from '@/components/max-width-wrapper'
 import { Phone } from '@/components/phone'
 import { Reviews } from '@/components/reviews'
+import { buttonVariants } from '@/components/ui/button'
 import { users } from '@/constants/users'
-import { Check, Star } from 'lucide-react'
+import { cn } from '@/utils/cn'
+import { ArrowRight, Check, Star } from 'lucide-react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -174,6 +177,76 @@ export default function Home() {
         <div className="pt-16">
           <Reviews />
         </div>
+      </section>
+
+      <section>
+        <MaxWidthWrapper className="py-24">
+          <div className="mb-12 px-6 lg:px-8">
+            <div className="mx-auto max-w-2xl sm:text-center">
+              <h2 className="order-1 mt-2 text-balance text-center text-5xl font-bold !leading-tight tracking-tight text-gray-900 md:text-6xl">
+                Upload your photo and get{' '}
+                <span className="relative bg-green-600 px-2 text-white">
+                  your own case{' '}
+                </span>{' '}
+                now
+              </h2>
+            </div>
+          </div>
+
+          <div className="mx-auto max-w-6xl px-6 lg:px-8">
+            <div className="relative flex grid-cols-2 flex-col items-center gap-40 md:grid">
+              <Image
+                src="/arrow.png"
+                alt="Arrow"
+                width={126}
+                height={31}
+                className="absolute left-1/2 top-[25rem] z-10 -translate-x-1/2 -translate-y-1/2 rotate-90 md:top-1/2 md:rotate-0"
+              />
+
+              <div className="relative h-80 max-w-sm rounded-xl bg-gray-900/5 ring-inset ring-gray-900/10 md:h-full md:justify-self-end lg:rounded-2xl">
+                <Image
+                  src="/horse.jpg"
+                  alt="Horse"
+                  width={384}
+                  height={576}
+                  className="size-full rounded-md bg-white object-cover shadow-2xl ring-1 ring-gray-900/10"
+                />
+              </div>
+
+              <Phone imgSrc="/horse_phone.jpg" className="w-60" />
+            </div>
+          </div>
+
+          <ul className="mx-auto mt-12 w-fit max-w-prose space-y-2 sm:text-lg">
+            <li className="w-fit">
+              <Check className="mr-1.5 inline size-5 text-green-600" />
+              High-quality silicone material
+            </li>
+            <li className="w-fit">
+              <Check className="mr-1.5 inline size-5 text-green-600" />
+              Scratch and fingerprint resistant coating
+            </li>
+            <li className="w-fit">
+              <Check className="mr-1.5 inline size-5 text-green-600" />
+              Wireless charging compatible
+            </li>
+            <li className="w-fit">
+              <Check className="mr-1.5 inline size-5 text-green-600" />5 year
+              print warranty
+            </li>
+
+            <div className="flex justify-center">
+              <Link
+                href="/configure/upload"
+                className={cn(
+                  buttonVariants({ size: 'lg', className: 'mx-auto mt-8' }),
+                )}
+              >
+                Create your case now <ArrowRight className="ml-1.5 size-4" />
+              </Link>
+            </div>
+          </ul>
+        </MaxWidthWrapper>
       </section>
     </div>
   )
---
+++ src/components/footer.tsx
@@ -0,0 +1,43 @@
+import Link from 'next/link'
+import { MaxWidthWrapper } from './max-width-wrapper'
+
+export function Footer() {
+  return (
+    <footer className="relative h-20 bg-white">
+      <MaxWidthWrapper>
+        <div className="border-t border-gray-200" />
+
+        <div className="flex h-full flex-col items-center justify-center md:flex-row md:justify-between">
+          <div className="pb-2 text-center md:pb-0 md:text-left">
+            <p className="text-sm text-muted-foreground">
+              &copy; {new Date().getFullYear()} All rights reserved
+            </p>
+          </div>
+
+          <div className="flex items-center justify-center">
+            <div className="flex space-x-8">
+              <Link
+                href="#"
+                className="text-sm text-muted-foreground hover:text-gray-600"
+              >
+                Terms
+              </Link>
+              <Link
+                href="#"
+                className="text-sm text-muted-foreground hover:text-gray-600"
+              >
+                Privacy Policy
+              </Link>
+              <Link
+                href="#"
+                className="text-sm text-muted-foreground hover:text-gray-600"
+              >
+                Cookie Policy
+              </Link>
+            </div>
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </footer>
+  )
+}
